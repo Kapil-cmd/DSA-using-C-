3ac58b477e78c8f76e8c3b0177bc1e4c690e3592
KAPIL KARKI <karkikapil228@gmail.com>
2025-03-16 00:04:04 +0000

    Stack Completed

--- AST/Stack/Stack.cs
+++ AST/Stack/Stack.cs
@@ -2,7 +2,7 @@
 {
     public class Stack
     {
-        public int MaxSize {  get; set; }
+        public int MaxSize { get; set; }
         public string[] StackArray { get; set; }
 
         public int Top { get; set; }
@@ -13,6 +13,33 @@
 
             this.StackArray = new string[MaxSize];
             Top = -1;
+        }
+        public void Push(string item)
+        {
+            Top++;
+            StackArray[Top] = item;
+        }
+        public string Pop()
+        {
+            //Think placeholder
+            int old_top = Top;
+            //Decrement for the new top
+            Top--;
+            return StackArray[old_top];
+        }
+        public string Peek()
+        {
+            return StackArray[Top];
+        }
+
+        public bool isEmpty()
+        {
+            return Top == 0;
+        }
+
+        public bool isFull()
+        {
+            return (MaxSize -1 == Top) ;
         }
     }
 }
--- Program.cs
+++ Program.cs
@@ -3,6 +3,23 @@ class Program
 {
     static void Main(string[] args)
     {
+        int size = 10;
+        Stack stack = new Stack(size);
+
+        for (int i = 0; i < 3; i++)
+        {
+            stack.Push("Ronaldo");
+            stack.Push("Bale");
+            stack.Push("Benzema");
+        }
+        stack.Pop();
+        stack.Peek();
+        while (!stack.isEmpty())
+        {
+            var value = stack.Pop();
+            Console.WriteLine(value);
+        }
+
     }
+
 }
- 
